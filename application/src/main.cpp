575ab4e7d0f04c82976f720bfb5ecb09879e9bb3
jonasvsc <jonasvasconcelosbarbosa@gmail.com>
2024-11-29 23:56:18 -0300

    command queue

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -65,8 +65,46 @@ int main(int argc, char** argv)
     wgpuDeviceSetUncapturedErrorCallback(device, onDeviceError, nullptr /* pUserData */);
 
     wgpuAdapterRelease(adapter);
-    
+
+    // GPU Queue
+
+    WGPUQueue queue = wgpuDeviceGetQueue(device);
+
+    auto onQueueWorkDone = [](WGPUQueueWorkDoneStatus status, void* userdata)
+    {
+        std::cout << "Queue work finished with status: " << status << '\n';
+    };
+    wgpuQueueOnSubmittedWorkDone(queue, onQueueWorkDone, nullptr);
+
+    // command encoder
+
+    WGPUCommandEncoderDescriptor encoderDesc{};
+    encoderDesc.nextInChain = nullptr;
+    encoderDesc.label = "My command encoder";
+    WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(device, &encoderDesc);
+
+    wgpuCommandEncoderInsertDebugMarker(encoder, "Do one thing");
+    wgpuCommandEncoderInsertDebugMarker(encoder, "Do another thing");
+
+    WGPUCommandBufferDescriptor cmdBufferDescriptor{};
+    cmdBufferDescriptor.nextInChain = nullptr;
+    cmdBufferDescriptor.label = "Command buffer";
+    WGPUCommandBuffer command = wgpuCommandEncoderFinish(encoder, &cmdBufferDescriptor);
+    wgpuCommandEncoderRelease(encoder);
+
+    std::cout << "Submiting command..." << std::endl;
+    wgpuQueueSubmit(queue, 1, &command);
+    wgpuCommandBufferRelease(command);
+    std::cout << "Command submited." << std::endl;
+
+    for (int i = 0; i < 5; ++i) 
+    {
+        std::cout << "Tick/Poll device..." << std::endl;
+        emscripten_sleep(100);
+    }
+
     wgpuDeviceRelease(device);
+    wgpuQueueRelease(queue);
     return 0;
 }
 
