c1844d1c875956d1564ba7b483a3fb3f597facb8
jonasvsc <jonasvasconcelosbarbosa@gmail.com>
2024-12-13 00:16:45 -0300

    imgui access uniforms

--- application/src/wgpu-renderer.cpp
+++ application/src/wgpu-renderer.cpp
@@ -69,7 +69,7 @@ void Renderer::render()
     renderPassColorAttachment.resolveTarget = nullptr;
     renderPassColorAttachment.loadOp = WGPULoadOp_Clear;
 	renderPassColorAttachment.storeOp = WGPUStoreOp_Store;
-    renderPassColorAttachment.clearValue = WGPUColor{ 0.05, 0.05, 0.05, 1.0 };
+    renderPassColorAttachment.clearValue = WGPUColor{ 1.0, 1.0, 1.0, 1.0 };
     renderPassColorAttachment.depthSlice = WGPU_DEPTH_SLICE_UNDEFINED;
 	renderPassDesc.colorAttachmentCount = 1;
 	renderPassDesc.colorAttachments = &renderPassColorAttachment;
@@ -488,7 +488,7 @@ void Renderer::initGui()
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGui::GetIO();
-
+    ImGui::StyleColorsDark();
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOther(window, true);
     ImGui_ImplWGPU_InitInfo init_info;
@@ -506,27 +506,15 @@ void Renderer::updateGui(WGPURenderPassEncoder renderPass)
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
+
     // [...] Build our UI
     // Build our UI
     static float f = 0.0f;
     static int counter = 0;
-    static bool show_demo_window = true;
-    static bool show_another_window = false;
-    static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    ImGui::Begin("Hello, world!");                                // Create a window called "Hello, world!" and append into it.
+    ImGui::Begin("Triangle Properties");                                
 
-    ImGui::Text("This is some useful text.");                     // Display some text (you can use a format strings too)
-    ImGui::Checkbox("Demo Window", &show_demo_window);            // Edit bools storing our window open/close state
-    ImGui::Checkbox("Another Window", &show_another_window);
-
-    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);                  // Edit 1 float using a slider from 0.0f to 1.0f
-    ImGui::ColorEdit3("clear color", (float*)&clear_color);       // Edit 3 floats representing a color
-
-    if (ImGui::Button("Button"))                                  // Buttons return true when clicked (most widgets return true when edited/activated)
-        counter++;
-    ImGui::SameLine();
-    ImGui::Text("counter = %d", counter);
+    ImGui::ColorEdit3("Color", (float*)&mUniforms.color);       // Edit 3 floats representing a color
 
     ImGuiIO& io = ImGui::GetIO();
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
